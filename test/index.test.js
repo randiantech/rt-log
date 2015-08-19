614dc168f13ccf767b3fef9a95bae6b7293fd329
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-08-19 10:20:42 -0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -10,7 +10,7 @@ describe('Tests', function () {
             }
         });
 
-        it('should correctly parse a description with one variable placeholder', function () {
+        it('should correctly parse description with one placeholder', function () {
             try {
                 var description = rtlog._processDescription('I0001', ['1234']);
                 assert(description.indexOf('1234') > -1);
@@ -19,7 +19,7 @@ describe('Tests', function () {
             }
         });
 
-        it('should correctly parse a description with two variable placeholders', function () {
+        it('should correctly parse description with two placeholders', function () {
             try {
                 var description = rtlog._processDescription('I0003', ['LEFT VALUE', 'RIGHT VALUE']);
                 assert(description.indexOf('LEFT VALUE') > -1);
